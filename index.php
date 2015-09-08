d5bfcce4c3d8b836ae49f257a9f609948d252dac
SimonDanger <dangersimon76@gmail.com>
2015-09-08 10:18:24 +0200

    Projet eglise

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8" />
+      <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
+      <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+
+      <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
+      <link rel="stylesheet" type="text/css" href="assets/css/slide.css" />
+      <script type="text/javascript" src="assets/js/responsiveslides.js"></script>
+      <script type="text/javascript" src="assets/js/responsiveslides.min.js"></script>   
+
+      <title>Eglise ...</title>
+
+   </head>
+
+   <body>
+      <?php include('slide.php'); ?>
+
+      <?php include('menu.php'); ?>
+      
+      <section>
+         <aside>
+
+         </aside>
+         <article>                
+
+         </article>
+      </section>
+      
+      <?php include('footer.php'); ?>
+   </body>
+</html>
---
+++ slide.php
@@ -0,0 +1,13 @@
+<script>
+	$(function() {
+  	$(".rslides").responsiveSlides();
+	});
+</script>
+
+<header>
+	<ul class="rslides">
+		<li><img src="assets/img/banniere.jpg" alt=""></li>
+		<li><img src="assets/img/Montireau001.jpg" alt=""></li>
+		<li><img src="assets/img/wip.jpg" alt=""></li>
+	</ul>
+</header>
