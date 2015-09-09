b215f4c1407f9877d2bfdd2204905122f4d71327
BenjminZak <benjaminzak21@gmail.com>
2015-09-09 18:47:25 +0200

    PLOP

--- slide.php
+++ slide.php
@@ -6,10 +6,9 @@
 
 <header>
 	<ul class="rslides">
-		<li><img src="assets/img/slide/#" alt=""></li>
-		<li><img src="assets/img/slide/#" alt=""></li>
-		<li><img src="assets/img/slide/#" alt=""></li>
-		<li><img src="assets/img/slide/#" alt=""></li>
-		<li><img src="assets/img/slide/#" alt=""></li>
+		<li><img src="assets/img/ext1.jpg" alt=""></li>
+		<li><img src="assets/img/status1.JPG" alt=""></li>
+		<li><img src="assets/img/vitre1.JPG" alt=""></li>
+		<li><img src="assets/img/vitre2.JPG" alt=""></li>
 	</ul>
 </header>
