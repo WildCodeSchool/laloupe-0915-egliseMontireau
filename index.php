6879c4b4d3b83273bc3073361be8ab78099c52c1
SimonDanger <dangersimon76@gmail.com>
2015-09-10 14:15:32 +0200

    mise a jour menu déroulant

--- index.php
+++ index.php
@@ -23,11 +23,47 @@
       
       <section>
          <aside>
-
          </aside>
          <article>                
-
          </article>
+         <div class ="presentation">
+            <article>                
+               <h2>Notre lutte en bref...</h2>
+
+               <p>L'association du "Comité de Sauvegarde de l'église de Montireau" oeuvre à la remise en 
+                  état de l'édifice locale, classé Monument Historique en 1980 (arrêté du 9 Juilet), et 
+                  tente de mettre en place un programme des travaux à réaliser accompagné d'un diagnostic 
+                  de l'opération (obligatoire pour les édifices classés).<br />
+                  Pour ce faire l'association fait appel à votre bienveillance afin de sauvegarder cette
+                  pépite du patrimoine culturel et religieux du XVIème siècle.<br />
+                  <br />
+                  Le Comité de Sauvegarde s'est également attaché aux objets mobilliers, en particulier
+                  la statuaire :
+                  <ul>
+                     <li>8 statues en pierre XVIème siècle inscrites sur l'inventaire supplémentaire 
+                        des objets mobiliers. Arrêté du 31 décembre 2009.</li>
+                     <li>5 statues en bois XVIème siècle inscrites sur l'inventaire supplémentaire 
+                        des objets mobiliers. Arrêté du 21 juillet 2011.</li>
+                     <li>Restauration de la Poutre de Gloire (aide parlementaire Laure de La Raudière).</li>
+                     <li>Restauration d'une statuette sous son dais, lauréate du concours 2011 organisé 
+                        par l'AMF et Arc-Nucléart. Le dossier a été préparé par le Comité de Sauvegarde.</li>
+                  </ul>
+                  D'important travaux restent à réaliser :
+                  <ul>
+                     <li>réfection de la charpente, de la couverture et restauration de la voûte lambrissée,</li>
+                     <li>remise en polychromie de la statuaire,</li>
+                     <li>traitement des peintures murales.</li>
+                  </ul>
+
+                  <a href="#">plus d'info...</a>
+               </p>
+
+            </article>
+
+            <aside>
+
+            </aside>
+         </div>
       </section>
       
       <?php include('footer.php'); ?>
---
+++ menu.php
@@ -0,0 +1,88 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mairie<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="wip.php">Accueil</a></li>
+            <li><a href="eglise.php">Eglise</a></li>
+            <li><a href="wip.php">Historique</a></li>
+            <li><a href="vitreau.php">Vitreau</a></li>
+            <li><a href="wip.php">Association</a></li>
+            <li><a href="wip.php">Contact</a></li>
+           <!--<li role="separator" class="divider"></li>-->
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cadre de Vie<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="eglise.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Intercommunalité et EPCI<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="wip.php">Interieur</a></li>
+            <li><a href="wip.php">Exterieur</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Economie<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="wip.php">s</a></li>
+            <li><a href="wip.php"</a></li>
+            <li><a href="wip.php">s</a></li>
+            <li><a href="wip.php"</a></li>
+            <li><a href="wip.php">e</a></li>
+            <li><a href="wip.php"</a></li>
+            <li><a href="wip.php"></a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Vie Associative<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Vie Scolaire<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Actions Sociales<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+          </ul>
+        </li>
+        <li><a href="wip.php"></a></li>
+        <li class="dropdown">
+          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Fil d'info<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+            <li><a href="wip.php"></a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
