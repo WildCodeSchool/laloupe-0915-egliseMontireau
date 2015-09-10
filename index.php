4f32e994a55e2057a9f6b581e179d53ce48c884e
SimonDanger <dangersimon76@gmail.com>
2015-09-10 15:13:16 +0200

    menu 0.3

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
       <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
       <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
 
+      <script type="text/javascript" src="assets/js/jquery-1.11.3.min.js"></script>
       <script type="text/javascript" src="assets/js/bootstrap.js"></script>
-      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 
       <link rel="stylesheet" type="text/css" href="assets/css/slide.css" />
       <script type="text/javascript" src="assets/js/responsiveslides.js"></script>
--- menu.php
+++ menu.php
@@ -5,83 +5,58 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="dropdown">
-          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mairie<span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Accueil<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="wip.php">Accueil</a></li>
-            <li><a href="eglise.php">Eglise</a></li>
-            <li><a href="wip.php">Historique</a></li>
-            <li><a href="vitreau.php">Vitreau</a></li>
-            <li><a href="wip.php">Association</a></li>
-            <li><a href="wip.php">Contact</a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
+            <li><a href="#.php"></a></li>
             <!--<li role="separator" class="divider"></li>-->
           </ul>
         </li>
         <li class="dropdown">
-          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cadre de Vie<span class="caret"></span></a>
+          <a href="#.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Eglise<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
           </ul>
         </li>
         <li class="dropdown">
-          <a href="eglise.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Intercommunalité et EPCI<span class="caret"></span></a>
+          <a href="eglise.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Historique<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="wip.php">Interieur</a></li>
-            <li><a href="wip.php">Exterieur</a></li>
+            <li><a href="#.php">Interieur</a></li>
+            <li><a href="#.php">Exterieur</a></li>
           </ul>
         </li>
         <li class="dropdown">
-          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Economie<span class="caret"></span></a>
+          <a href="#.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Vitraux<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="wip.php">s</a></li>
-            <li><a href="wip.php"</a></li>
-            <li><a href="wip.php">s</a></li>
-            <li><a href="wip.php"</a></li>
-            <li><a href="wip.php">e</a></li>
-            <li><a href="wip.php"</a></li>
-            <li><a href="wip.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
           </ul>
         </li>
         <li class="dropdown">
-          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Vie Associative<span class="caret"></span></a>
+          <a href="#.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Association<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
           </ul>
         </li>
         <li class="dropdown">
-          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Vie Scolaire<span class="caret"></span></a>
+          <a href="#.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Actions Sociales<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-          </ul>
-        </li>
-        <li><a href="wip.php"></a></li>
-        <li class="dropdown">
-          <a href="wip.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Fil d'info<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
-            <li><a href="wip.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
+            <li><a href="#.php"></a></li>
           </ul>
         </li>
       </ul>
