c4218d710db4476c181425cf117ce940ad6a0b46
NicoRdr <nicolas.rodrigues94@gmail.com>
2015-09-11 17:07:18 +0200

    formulaire de contact

---
+++ contact.php
@@ -0,0 +1,28 @@
+<div>
+	<form method="post" action="traitement.php">
+   	
+   	 <p>
+       <label for="M/Mme/Melle">Nom et Prénom:</label>
+       <input type="#" name="p#" id="#" />
+       
+       <br />
+       <label for="Adresse">adresse :</label>
+       <input type="#" name="#" id="#" />
+       
+
+
+   <p>
+       <label for="ameliorer">
+       Faire un don pour la Sauvegarde d'un monument histoirique 
+       </label>
+       <br />
+       
+       <textarea name="ameliorer" id="ameliorer" rows="10" cols="50">
+      
+       </textarea>       
+   </p>
+</form>
+
+
+
+</div>
