48860d6660f6c08a8d7e8a82fb68d34540d39aa3
BenjminZak <benjaminzak21@gmail.com>
2015-09-14 12:00:01 +0200

    ajout du slide

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8" />
+      <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
+      <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+
+      <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
+      <link rel="stylesheet" type="text/css" href="assets/css/slide.css" />
+      <script type="text/javascript" src="assets/js/responsiveslides.js"></script>
+      <script type="text/javascript" src="assets/js/responsiveslides.min.js"></script>   
+
+      <title>Comité de Sauvegarde de l'Eglise de Montireau</title>
+
+   </head>
+
+   <body>
+      <?php include('slide.php'); ?>
+
+      <?php include('menu.php'); ?>
+      
+      <section>
+         
+         <article>                
+
+         </article>
+
+         <aside>
+
+         </aside>
+
+      </section>
+      
+      <?php include('footer.php'); ?>
+   </body>
+</html>
---
+++ slide.php
@@ -0,0 +1,14 @@
+<script>
+	$(function() {
+  	$(".rslides").responsiveSlides();
+	});
+</script>
+
+<header>
+	<ul class="rslides">
+		<li><img src="assets/img/photo1.JPG" alt=""></li>
+		<li><img src="assets/img/photo2.JPG" alt=""></li>
+		<li><img src="assets/img/photo3.JPG" alt=""></li>
+		<li><img src="assets/img/photo4.JPG" alt=""></li>
+	</ul>
+</header>
