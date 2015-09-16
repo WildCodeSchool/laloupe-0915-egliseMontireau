5e3da38d4af54ed3d76f7a67177adeb77fdc0ca3
BenjminZak <benjaminzak21@gmail.com>
2015-09-16 15:03:30 +0200

    maj slide

--- slide.php
+++ slide.php
@@ -6,9 +6,9 @@
 
 <header>
 	<ul class="rslides">
-		<li><img src="assets/img/ext1.jpg" alt=""></li>
-		<li><img src="assets/img/status1.JPG" alt=""></li>
-		<li><img src="assets/img/vitre1.JPG" alt=""></li>
-		<li><img src="assets/img/vitre2.JPG" alt=""></li>
+		<li><img src="assets/img/slide/ext1.jpg" alt=""></li>
+		<li><img src="assets/img/slide/vitre2.jpg" alt=""></li>
+		<li><img src="assets/img/slide/statue1.jpg" alt=""></li>
+		<li><img src="assets/img/slide/vitre1.jpg" alt=""></li>
 	</ul>
 </header>
